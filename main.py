51704c3bee1fa1b85ad2311b833061e46de231f7
utk468 <utkarshpise468@gmail.com>
2026-03-16 18:11:19 +0530

    third edit sse add

--- main.py
+++ main.py
@@ -244,7 +244,7 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
 
     mcp.run(
-        transport="http",
+        transport="sse",
         host="0.0.0.0",
         port=port
     )
